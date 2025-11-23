179a738eb68d7c6c93ca2387001542cdb5105a28
Min Pyae Hein <minpyaehein536h@prestigein.com>
2025-11-23 22:11:57 +0700

    first commit

---
+++ src/test/java/com/autowise/demo/CommonBackendJewelleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autowise.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonBackendJewelleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
