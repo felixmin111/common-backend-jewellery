c86d1e04a0e99b0d3cdcbe10099ceb5f5e5adce4
Min Pyae Hein <minpyaehein536h@prestigein.com>
2025-11-23 22:11:57 +0700

    first commit

---
+++ src/main/java/com/autowise/demo/CommonBackendJewelleryApplication.java
@@ -0,0 +1,13 @@
+package com.autowise.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonBackendJewelleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonBackendJewelleryApplication.class, args);
+	}
+
+}
