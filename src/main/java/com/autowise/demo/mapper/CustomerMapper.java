30fe5b07e3276b3b065bcb649b8ffe2ba5542580
wy107 <u6511488@au.edu>
2025-12-03 14:40:44 +0700

    toe-backend

--- src/main/java/com/autowise/demo/mapper/CustomerMapper.java
+++ src/main/java/com/autowise/demo/mapper/CustomerMapper.java
@@ -18,7 +18,6 @@ public interface CustomerMapper {
     @Mapping(target = "id", ignore = true)
     Customer toEntity(CustomerDto dto);
 
-    // DTO -> existing Entity (for update, only non-null fields)
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     @Mapping(source = "password", target = "hashPassword")
     void updateEntityFromDto(CustomerDto dto, @MappingTarget Customer entity);
