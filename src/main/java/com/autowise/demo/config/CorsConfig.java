0f2e6079c712d45eea685638b2484afecd861bfa
Min Pyae Hein <minpyaehein536h@prestigein.com>
2026-01-30 00:03:53 +0700

    fix cross config

--- src/main/java/com/autowise/demo/config/CorsConfig.java
+++ src/main/java/com/autowise/demo/config/CorsConfig.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
-    @Value("${app.cors.allowed-origins:http://localhost:5173}")
+    @Value("${app.cors.allowed-origins:}")
     private String allowedOrigins;
 
     private String[] origins() {
